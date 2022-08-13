b4e2c467df3d492c26fb4ef38f764a261146ffe3
Timo <developer.timo@gmail.com>
2022-08-13 10:53:05 +0900

    [JPA 시작하기] Hello JPA - 애플리케이션 개발
    - Member 테이블에 데이터 INSERT 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,13 +2,29 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
+        //엔티티 매니저 팩토리는 어플리케이션에 하나만 있어야 함
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        //엔티티 매니저는 DB 세션 단위(트랜잭션 단위)로 생성해주어야 함
         EntityManager em = emf.createEntityManager();
+
+        //JPA에서는 트랜잭션 단위가 중요함. 작업 수행은 반드시 트랜잭션 내에서 이루어져야 한다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("HelloA");
+
+        em.persist(member);
+
+        tx.commit();
+
         em.close();
         emf.close();
     }
