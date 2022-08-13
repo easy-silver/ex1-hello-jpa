27ccc64f2314d8318f5e763e881b1c134bea044c
Timo <developer.timo@gmail.com>
2022-08-13 11:46:59 +0900

    add comment.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,12 +8,12 @@ import javax.persistence.Persistence;
 public class JpaMain {
 
     public static void main(String[] args) {
-        //엔티티 매니저 팩토리는 어플리케이션에 하나만 있어야 함
+        //엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유한다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        //엔티티 매니저는 DB 세션 단위(트랜잭션 단위)로 생성해주어야 함
+        //엔티티 매니저는 DB 세션 단위(트랜잭션 단위)로 생성해주어야 하며, 쓰레드간에 공유하면 안된다.
         EntityManager em = emf.createEntityManager();
 
-        //JPA에서는 트랜잭션 단위가 중요함. 작업 수행은 반드시 트랜잭션 내에서 이루어져야 한다.
+        //JPA에서는 트랜잭션 단위가 중요함. JPA의 모든 변경은 트랜잭션 안에서 실행되어야 한다.
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
