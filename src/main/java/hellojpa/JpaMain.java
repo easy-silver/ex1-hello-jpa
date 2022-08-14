41c2684c5d47d0915bc44ed25a9d3464ed98ab2e
Timo <developer.timo@gmail.com>
2022-08-14 21:49:46 +0900

    [영속성 관리 - 내부 동작 방식] 영속성 컨텍스트 1
    - 엔티티 저장 시 SQL 전송 시점 확인을 통한 영속 상태에 대한 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -19,16 +18,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //JPA(JPQL)를 사용하면 검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색한다.
-            List<Member> members = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for (Member member : members) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            System.out.println("=== before persist");
+            //영속
+            em.persist(member);
+
+            System.out.println("=== before commit ");
             tx.commit();
+
+            //SQL이 DB로 보내지는 시점은 commit 이후이다.
+            System.out.println("=== after commit");
         } catch (Exception e) {
             tx.rollback();
         } finally {
